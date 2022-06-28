9541cf44e11f0acfda4eda8fccdeedda539023b1
SamuelSG <samuel.sanchezg@hotmail.com>
2022-06-27 19:18:28 -0500

    Update main.cpp

--- Semana 1/Sesión 1/Actividad 1/main.cpp
+++ Semana 1/Sesión 1/Actividad 1/main.cpp
@@ -1,5 +1,5 @@
 /*
-  compilar: 
+  compilar:
     g++ -std=c++17 -Wall *.cpp
 
   ejecutar:
@@ -9,11 +9,18 @@
 #include <iostream>
 #include "ListaTemplate.h"
 
-int main() {
+int main()
+{
   std::cout << "Template example!\n";
   ListaTemplate<int> myList;
 
   myList.insert(15);
-  
+  myList.insert(12);
+  myList.insert(22);
+  myList.print();
+  std::cout << myList.getSize() << std::endl;
+  // myList.erase();
+  myList.getData(3);
+
   return 0;
 }
