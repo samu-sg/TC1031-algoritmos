e77c0e2485b7aa2d94c9d4db18ac36107425a628
SamuelSG <samuel.sanchezg@hotmail.com>
2022-06-27 19:17:58 -0500

    Update ListaTemplate.h

--- Semana 1/Sesión 1/Actividad 1/ListaTemplate.h
+++ Semana 1/Sesión 1/Actividad 1/ListaTemplate.h
@@ -22,12 +22,14 @@ public:
   void print();
 };
 
+// Constructor
 template <class T>
 ListaTemplate<T>::ListaTemplate()
 {
   currentSize = 0;
 }
 
+// Recibe el valor del número que se desea insertar; si hay espacio disponible, se coloca al final del vector y se incrementa en 1 size.
 template <class T>
 bool ListaTemplate<T>::insert(T value)
 {
@@ -41,6 +43,7 @@ bool ListaTemplate<T>::insert(T value)
     return false;
 }
 
+// Si el vector no está vacío, despliega el último elemento del vector (el que está en la posición size-1) y disminuye en 1 el tamaño de size. Si el vector está vacío, despliega el mensaje: “NO HAY ELEMENTOS”.
 template <class T>
 void ListaTemplate<T>::erase()
 {
@@ -54,27 +57,34 @@ void ListaTemplate<T>::erase()
     std::cout << "NO HAY ELEMENTOS" << std::endl;
 }
 
+// Recibe la posición de donde se desea obtener la información, y regresa el contenido del vector en esa posición. La precondición es que la posición sea un valor existente dentro de los datos
 template <class T>
 T ListaTemplate<T>::getData(int indice)
 {
-  if (0 <= indice < currentSize)
+  if (0 <= indice && indice < currentSize)
   {
-    std::cout << data(indice) << std::endl;
+    std::cout << data[indice] << std::endl;
+  }
+  else
+  {
+    std::cout << "No hay nada" << std::endl;
   }
 }
 
+//  Regresa el valor del atributo size.
 template <class T>
 int ListaTemplate<T>::getSize()
 {
   return currentSize;
 }
 
+// Imprime la lista de datos, uno por línea
 template <class T>
 void ListaTemplate<T>::print()
 {
   for (int indice = 0; indice < currentSize; indice++)
   {
-    std::cout << indice << " - " << data(indice) << std::endl;
+    std::cout << indice << " - " << data[indice] << std::endl;
   }
 }
 
