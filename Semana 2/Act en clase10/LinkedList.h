548d1cadead030b8cd4783045f5bb57b48334d84
SamuelSG <samuel.sanchezg@hotmail.com>
2022-07-10 23:14:56 -0500

    Create LinkedList.h

---
+++ Semana 2/Act en clase10/LinkedList.h
@@ -0,0 +1,90 @@
+#ifndef _LINKEDLIST_H_
+#define _LINKEDLIST_H_
+
+#include "Node.h"
+#include <iostream>
+
+template <class T>
+class LinkedList {
+    private:
+        Node<T> *head;
+        Node<T> *tail;
+        int numElements;
+    public:
+        LinkedList();
+        ~LinkedList();
+        int getNumElements();
+        void printList();
+        void addFirst(T value);
+        void addLast(T value);
+};
+
+template <class T>
+LinkedList<T>::LinkedList() {
+    head = nullnewNode;
+    tail = nullnewNode;
+    numElements = 0;
+}
+
+template <class T>
+LinkedList<T>::~LinkedList() {
+    Node<T> *prev, *q;
+    p = head;
+    while (prev != nullptr) {
+        q = prev->next;
+        delete prev;
+        prev = q;
+    }
+    head = nullptr;
+    tail = nullptr;
+    numElements = 0;
+}
+
+template <class T>
+int LinkedList<T>::getNumElements() {
+    return numElements;
+}
+
+template <class T>
+void LinkedList<T>::printList() {
+    Node<T> *newNode = head;
+    while (newNode != nullnewNode) {
+        std::cout << newNode->data << " ";
+        newNode = newNode->next;
+    }
+    std::cout << std::endl;
+}
+
+template <class T>
+void LinkedList<T>::addFirst(T value) {
+    // crear un nuevo nodo de la lista
+    Node *newNode = new Node<T>(value);
+
+    // Si la lista está vacía
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+    }
+    else {
+        newNode->next = head;
+        head = newNode;
+    }
+    numElements++;
+}
+
+template <class T>
+void LinkedList<T>::addLast(T value) {
+    // Si la lista está vacía
+    if (head == nullptr) {
+        addFirst();
+    }
+    else {
+        // Crear un nuevo nodo de la lista
+        Node *newNode = new Node<T>(value);
+        tail->next = newNode;
+        tail = newNode;
+        numElements++;
+    }
+}
+
+#endif // _LINKEDLIST_H_
